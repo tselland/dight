6b505e660d0239965c895741cce26f29edb6aa3c
tselland <tselland@byu.edu>
2016-03-31 09:32:57 -0600

    jquery

--- website/js/script.js
+++ website/js/script.js
@@ -3,8 +3,10 @@
  */
 
 $(function() {
-    $(".toggle_nav").on('click.menuOpen', function(){
-        $(".flex_nav ul").slideToggle();
+    $(".menu_show").on('click.menuOpen', function(){
+        $(".flex_nav ul").slideDown().addClass("menu_show");
+    }, function(){
+        $(".flex_nav ul").slideDown().removeClass("menu_show");
     });
 
     $("header h1").fadeIn(1000);
