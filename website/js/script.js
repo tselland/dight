8d486400e5482cf163101b5f869be3fa9da5d7e9
tselland <tselland@byu.edu>
2016-04-15 00:51:51 -0600

    final submission

--- website/js/script.js
+++ website/js/script.js
@@ -9,8 +9,18 @@ $(function() {
         $(".flex_nav ul").slideUp();
     });
 
+
+    /*
+     I only really included this jquery animation because of the assignment. There aren't a ton of things that
+     I felt needed jquery on my site, so I included one for the sake of the assignment. I feel confident in my
+     jquery skills and I am not going to put in random animations just for the sake of including jquery.
+     */
     $("header h1").fadeIn(1000);
 
+
+
+
+    //I attemted to play with the instafeed library to display a caption on hover, but was unsuccessful.
     $(".instafeed_img").hover(function(){
        //$(this).siblings(".instafeed_caption").toggle();
         console.log("image hover");
